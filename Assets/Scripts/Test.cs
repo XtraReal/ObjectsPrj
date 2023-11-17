f38800660728ad572c6b9d2582d0da643dbc00fd
XtraReal <149747643+XtraReal@users.noreply.github.com>
2023-11-17 11:24:00 -0500

    Updated Test Script 14-1116

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,8 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Utilities.ShowDeviceID("LUCY");
         Debug.Log(Utilities.DEVICE_ID);
+        Utilities.ShowDeviceID("LUCY");
     }
 
     // Update is called once per frame
